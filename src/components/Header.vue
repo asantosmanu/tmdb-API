f8eaa45a700d5e859cb769778cabff4f59f1ac88
Bruna Henz <brunaehenz@gmail.com>
2023-11-13 11:13:06 -0300

    Alterar Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,35 @@
 <template>
   <header>
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/filmes">Filmes</router-link>
-      <router-link to="/tv">Programas de TV</router-link>
+      <router-link class="home" to="/">Home</router-link>
+      <router-link class="filme" to="/filmes">Filmes</router-link>
+      <router-link class="TV" to="/tv">Séries</router-link>
     </nav>
   </header>
 
 </template>
+<style scoped>
+
+nav{
+  display: flex;
+  align-items: center;
+  color: aliceblue;
+  font-family:Arial, Helvetica, sans-serif;
+  
+}
+.home{
+  margin-left: 690px;
+}
+.filme{
+  margin-left: 10%;
+}
+.TV{
+  margin-left: 10%;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+</style>
