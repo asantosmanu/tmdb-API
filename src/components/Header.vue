e793d0c5b60fa2dcc73875adb27d6e703bb498fb
miguelmartix <miguel.martins1030@gmail.com>
2023-11-27 11:12:51 -0300

    logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,7 @@
 <template>
   <header>
     <nav>
+      <div class="logo">    <img src="../assets/logo.png" alt=""></div>
       <router-link class="home" to="/">Home</router-link>
       <router-link class="filme" to="/filmes">Filmes</router-link>
       <router-link class="TV" to="/tv">Séries</router-link>
@@ -14,8 +15,18 @@
 
 </template>
 <style scoped>
+.logo{
+  width: 100%;
+  display: flex;
+  height: 15%;
+  margin-top: 1%;
+  margin-right: 10%;
+  margin-left: -200px;
 
+
+}
 nav{
+  margin-top: -39%;
   display: flex;
   align-items: center;
   color: aliceblue;
@@ -23,13 +34,13 @@ nav{
   
 }
 .home{
-  margin-left: 690px;
+  margin-right: 5%;
 }
 .filme{
-  margin-left: 10%;
+  margin-right: 5%;
 }
 .TV{
-  margin-left: 10%;
+  margin-right: 5%;
 }
 
 a {
