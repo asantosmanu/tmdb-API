9e9ac7e22b54c0b1491ae5d293ad371de53873c3
Vi140906 <santosviih360@gmail.com>
2023-12-04 13:56:42 -0300

    home e carosel

--- src/_data/img.js
+++ src/_data/img.js
@@ -1,48 +1,30 @@
-import { ref} from 'vue'
-
+import {ref} from 'vue'
 const imgs = ref(
-    [
-      {
-        src: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQgjn5RF4tUM5CHMRP-vbNTqWxs9pFqjPLuKa4VDP81MHN-MZG9',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://hqrock.files.wordpress.com/2013/05/justice-league-banner-by-jim-lee-new-52.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://hqrock.files.wordpress.com/2013/05/justice-league-banner-by-jim-lee-new-52.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://hqrock.files.wordpress.com/2013/05/justice-league-banner-by-jim-lee-new-52.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://hqrock.files.wordpress.com/2013/05/justice-league-banner-by-jim-lee-new-52.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://hqrock.files.wordpress.com/2013/05/justice-league-banner-by-jim-lee-new-52.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://m.media-amazon.com/images/I/61Fo3CitEGL._AC_UF1000,1000_QL80_.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://www.casaarteesaber.com.br/public-images/product/zoom-59-42C88.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://capas-p.imagemfilmes.com.br/164908_000_p.jpg',
-        alt: 'Not Filme'
-      },
-      {
-        src: 'https://lh4.googleusercontent.com/xbryw1wWK0SAxsKaNRX7QnMcaTWTZg9s24y_g-XtpPtOTNE6I5MWN_6rLrBgokcPYawGYMR8QMGurel-E4k5sn2qnjiV7Ru9ulQuBowuC3aUhbc0Y8FOzrnQGZRA-94wYr44ja1XH6M2306IDg',
-        alt: 'Not Filme'
-      },
-  
-    ]
-  )
+  [
+    {
+      src: 'https://pixarbrasilblog.com.br/wp-content/uploads/2020/05/Capa_Wordpress_DoisIrmaos_AmazonPrime-720x340.png',
+      alt: 'Not Filme'
+    },
+    {
+      src: 'https://pixarbrasilblog.com.br/wp-content/uploads/2020/05/Capa_Wordpress_DoisIrmaos_AmazonPrime-720x340.png',
+      alt: 'Not Filme'
+    },
+    {
+      src: 'https://pixarbrasilblog.com.br/wp-content/uploads/2021/11/Capa_Wordpress-Maior_Red-Trailer-720x340.jpg',
+      alt: 'Not Filme'
+    },
+    {
+      src: 'https://pixarbrasilblog.com.br/wp-content/uploads/2020/05/Template-Imagem_Capa_Wordpress_Logo-Viva-720x340.jpg',
+      alt: 'Not Filme'
+    },
+    {
+      src: 'https://pixarbrasilblog.com.br/wp-content/uploads/2020/05/Capa_Wordpress_DoisIrmaos_AmazonPrime-720x340.png',
+      alt: 'Not Filme'
+    },
+    {
+      src: 'https://pixarbrasilblog.com.br/wp-content/uploads/2020/05/Capa_Wordpress_DoisIrmaos_AmazonPrime-720x340.png',
+      alt: 'Not Filme'
+    },
+  ]
+)
 export {imgs}
